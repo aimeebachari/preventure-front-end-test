7eed68b037aa82c8162f4ad4744df1abd5cb4f66
Aimee Bachari <aimeebachari@gmail.com>
2017-05-23 21:15:03 -0400

    deleted get and index actions in api controller

--- app/controllers/api/v1/gyms_controller.rb
+++ app/controllers/api/v1/gyms_controller.rb
@@ -1,14 +1,6 @@
 class Api::V1::GymsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def get
-
-  end
-
-  def index
-
-  end
-
   def show
     uri = URI("http://fe-test.preventure.com/api/v1/gyms/")
     response = Net::HTTP.get_response(uri)

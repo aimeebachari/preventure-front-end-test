93d43919cf7a7098d415edb5f0c7bade1e4d31e6
Aimee Bachari <aimeebachari@gmail.com>
2017-05-30 09:24:14 -0400

    more saying, added chain

--- react/src/components/Gym.js
+++ react/src/components/Gym.js
@@ -4,18 +4,26 @@ import { Callout } from 'react-foundation';
 class Gym extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
 
   render() {
+    let chain;
+    if (this.props.chain === "1") {
+      chain = <p>This gym is part of a chain</p>;
+    }
+
     return (
-      <div className="row container small-6 large-12 columns">
+      <div className="row container small-6 large-12 columns"><br />
         <Callout id="gyms-index">
           <p>
             <strong>{this.props.name}</strong><br />
             {this.props.street}<br />
             {this.props.city}, {this.props.state} {this.props.zip}
           </p>
-        </Callout>
+            {chain}
+        </Callout><br />
       </div>
     );
   }
